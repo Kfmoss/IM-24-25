929b6defb3d9c48c82cfaae4d92a329d08c4d8b6
Kfmoss <berlin1980e@gmail.com>
2024-06-21 05:26:48 +0200

    geting ready

---
+++ code/index.js
@@ -0,0 +1,13 @@
+
+function menuMain(){
+    const menu = document.getElementById("block_menu");
+    if(menu.style.display !='block'){
+        menu.style.display = 'block';
+    }
+    else{
+        menu.style.display= 'none';
+       
+    }
+    
+
+}
