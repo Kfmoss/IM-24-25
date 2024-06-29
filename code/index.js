207c7911ad18fd8165fd7e3b2df4eb54a948f035
Kfmoss <berlin1980e@gmail.com>
2024-06-29 10:31:22 +0200

    menu ready

--- code/index.js
+++ code/index.js
@@ -11,3 +11,63 @@ function menuMain(){
     
 
 };
+function menuOppgave1(){
+    const menu = document.getElementById("oppgave1");
+    if(menu.style.display !='block'){
+        menu.style.display = 'block';
+    }
+    else{
+        menu.style.display= 'none';
+       
+    }
+    
+
+};
+function menuOppgave2(){
+    const menu = document.getElementById("oppgave2");
+    if(menu.style.display !='block'){
+        menu.style.display = 'block';
+    }
+    else{
+        menu.style.display= 'none';
+       
+    }
+    
+
+};
+function menuOppgave3(){
+    const menu = document.getElementById("oppgave3");
+    if(menu.style.display !='block'){
+        menu.style.display = 'block';
+    }
+    else{
+        menu.style.display= 'none';
+       
+    }
+    
+
+};
+function menuOppgave4(){
+    const menu = document.getElementById("oppgave4");
+    if(menu.style.display !='block'){
+        menu.style.display = 'block';
+    }
+    else{
+        menu.style.display= 'none';
+       
+    }
+    
+
+};
+function menuOppgave5(){
+    const menu = document.getElementById("oppgave5");
+    if(menu.style.display !='block'){
+        menu.style.display = 'block';
+    }
+    else{
+        menu.style.display= 'none';
+       
+    }
+    
+
+};
