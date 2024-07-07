554ac626f7bda01516e9cc34c127a248a83fcd58
Kfmoss <berlin1980e@gmail.com>
2024-07-07 14:28:34 +0200

    menu

--- code/index.js
+++ code/index.js
@@ -71,3 +71,16 @@ function menuOppgave5(){
     
 
 };
+
+const hamMenu = document.querySelector('.imsk_btnmenu');
+const offScreenMenu = document.querySelector('.imsk-off-screen-menu');
+
+hamMenu.addEventListener('click', () => {offScreenMenu.classList.toggle('active')});
+document.addEventListener('click',(e) =>{
+    if(!offScreenMenu.contains(e.target)  && !hamMenu.contains(e.target)){
+        offScreenMenu.classList.remove('active');
+    }
+} )
+
+
+
