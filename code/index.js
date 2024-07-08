18d66f35a9af1121e80409c124ac835f0d1e35c2
Kfmoss <berlin1980e@gmail.com>
2024-07-08 10:17:34 +0200

    almost done

--- code/index.js
+++ code/index.js
@@ -1,76 +1,4 @@
 
-function menuMain(){
-    const menu = document.getElementById("block_menu");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
-       
-    }
-    
-
-};
-function menuOppgave1(){
-    const menu = document.getElementById("oppgave1");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
-       
-    }
-    
-
-};
-function menuOppgave2(){
-    const menu = document.getElementById("oppgave2");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
-       
-    }
-    
-
-};
-function menuOppgave3(){
-    const menu = document.getElementById("oppgave3");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
-       
-    }
-    
-
-};
-function menuOppgave4(){
-    const menu = document.getElementById("oppgave4");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
-       
-    }
-    
-
-};
-function menuOppgave5(){
-    const menu = document.getElementById("oppgave5");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
-       
-    }
-    
-
-};
 
 const hamMenu = document.querySelector('.imsk_btnmenu');
 const offScreenMenu = document.querySelector('.imsk-off-screen-menu');
@@ -82,14 +10,6 @@ document.addEventListener('click',(e) =>{
     }
 } )
 
-const hamMenu_main = document.querySelector('.main_btnmenu');
-const offscreenMenu_main = document.querySelector('.main-off-screen-menu');
 
-hamMenu_main.addEventListener('click', () => {offscreenMenu_main.classList.toggle('active')});
-document.addEventListener('click',(e) =>{
-    if(!offscreenMenu_main.contains(e.target)  && !hamMenu_main.contains(e.target)){
-        offscreenMenu_main.classList.remove('active');
-    }
-} )
 
 
