56d8ff71acd824b6161fbfc369d105a94110af28
Kfmoss <berlin1980e@gmail.com>
2024-07-09 11:24:45 +0200

    please

--- code/main.js
+++ code/main.js
@@ -15,76 +15,78 @@ document.addEventListener('click',(e) =>{
 } )
 const op3 = document.querySelector('.op3');
 const op3_list = document.querySelector('.oppgave3');
+const op1 = document.querySelector('.op1');
+const op1_list = document.querySelector('.oppgave1');
+const op2 = document.querySelector('.op2');
+const op2_list = document.querySelector('.oppgave2');
+const op4 = document.querySelector('.op4');
+const op4_list = document.querySelector('.oppgave4');
+const op5 = document.querySelector('.op5');
+const op5_list = document.querySelector('.oppgave5');
+
 op3.addEventListener('click', ()=>{
     if (op3_list.style.display !='block'){
         op3_list.style.display ='block';
         
     }
 })
+op1.addEventListener('click', ()=>{
+    if (op1_list.style.display !='block'){
+        op1_list.style.display ='block';
+        
+    }
+})
+op2.addEventListener('click', ()=>{
+    if (op2_list.style.display !='block'){
+        op2_list.style.display ='block';
+        
+    }
+})
+op4.addEventListener('click', ()=>{
+    if (op4_list.style.display !='block'){
+        op4_list.style.display ='block';
+        
+    }
+})
+op5.addEventListener('click', ()=>{
+    if (op5_list.style.display !='block'){
+        op5_list.style.display ='block';
+        
+    }
+})
 document.addEventListener('click', (ex)=>{
     if(!op3_list.contains(ex.target)&& !op3.contains(ex.target)){
         op3_list.style.display ='none';
-
+    }
+    if(!op1_list.contains(ex.target)&& !op1.contains(ex.target)){
+        op1_list.style.display ='none';
+    }
+    if(!op2_list.contains(ex.target)&& !op2.contains(ex.target)){
+        op2_list.style.display ='none';
+    }
+    if(!op4_list.contains(ex.target)&& !op4.contains(ex.target)){
+        op4_list.style.display ='none';
+    }
+    if(!op5_list.contains(ex.target)&& !op5.contains(ex.target)){
+        op5_list.style.display ='none';
     }
 })
 
-function menuOppgave1(){
-    const menu = document.getElementById("oppgave1");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
+
+
+
+
+
+
+// function menuOppgave1(){
+//     const menu = document.getElementById("oppgave1");
+//     if(menu.style.display !='block'){
+//         menu.style.display = 'block';
+//     }
+//     else{
+//         menu.style.display= 'none';
        
-    }
+//     }
     
 
-};
-function menuOppgave2(){
-    const menu = document.getElementById("oppgave2");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
-       
-    }
-    
-
-};
-function menuOppgave3(){
-    const menu = document.getElementById("oppgave3");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
-       
-    }
-    
-
-};
-function menuOppgave4(){
-    const menu = document.getElementById("oppgave4");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
-       
-    }
-    
-
-};
-function menuOppgave5(){
-    const menu = document.getElementById("oppgave5");
-    if(menu.style.display !='block'){
-        menu.style.display = 'block';
-    }
-    else{
-        menu.style.display= 'none';
-       
-    }
-    
-
-};
+// };
